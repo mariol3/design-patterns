3349b4d71bd68ace42b34c24faff2372fccd6058
Mario Grimaldi <mario.grimaldi89@gmail.com>
2015-06-21 17:25:22 +0200

    Abbiamo reso dinamico il set dei comportamenti per un anatra tramite dei setter per quack e fly behavior.

--- strategy/src/main/java/it/mario/simuduck/Main.java
+++ strategy/src/main/java/it/mario/simuduck/Main.java
@@ -1,5 +1,6 @@
 package it.mario.simuduck;
 
+import it.mario.simuduck.behaviors.fly.impl.FlyWithRocket;
 import it.mario.simuduck.ducks.MallardDuck;
 import it.mario.simuduck.ducks.RubberDuck;
 
@@ -9,17 +10,10 @@ import it.mario.simuduck.ducks.RubberDuck;
 public class Main {
 
     public static void main(String[] args) {
-        MallardDuck mallardDuck = new MallardDuck();
         RubberDuck rubberDuck = new RubberDuck();
 
-        System.out.println("A mallard duck");
-        mallardDuck.performQuack();
-        mallardDuck.performFly();
-
-        System.out.println("");
-
-        System.out.println("A rubber duck");
-        rubberDuck.performQuack();
+        rubberDuck.performFly();
+        rubberDuck.setFlyBehavior(new FlyWithRocket());
         rubberDuck.performFly();
     }
 

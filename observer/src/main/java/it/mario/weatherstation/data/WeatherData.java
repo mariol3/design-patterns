d4bc44847b50ebd1a0beaa5460c1001164ff0835
Mario Grimaldi <mario.grimaldi89@gmail.com>
2015-06-22 18:08:36 +0200

    Pattern Observer. Spiegazione del problema di esempio.

---
+++ observer/src/main/java/it/mario/weatherstation/data/WeatherData.java
@@ -0,0 +1,30 @@
+package it.mario.weatherstation.data;
+
+/**
+ * Created by Mario Grimaldi <mario.grimaldi89@gmail.com> with <3
+ */
+public class WeatherData {
+
+    double temperature;
+    double pressure;
+    double humidity;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public double getHumidity() {
+        return humidity;
+    }
+
+    /*
+     * Ehi! Questo metodo va implementato! Your job ;)
+     */
+    public void measurementsChanged() {
+        // TODO implementare
+    }
+}
